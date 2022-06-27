985fbb5d6c6e3e32a29c21f7d300c7033a2e1033
Edi6758 <ediarts7@gmail.com>
2022-06-27 02:22:22 -0300

    1) criando todas as entidades da parte de documentos do projto, DOCUMENTO, CPF, RG e TITULO DE ELEITOR. \n2)Criando estrutura inicial dos controles e das telas de documentos.

---
+++ entidades/Documento.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class Documento(ABC):
+    @abstractmethod
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(selfself, nome: str):
+        self.__nome = nome
